582085476349c74d7a76cd3419ff824bc7c00638
EC2 Default User <ec2-user@ip-172-31-21-43.ap-northeast-1.compute.internal>
2024-06-30 06:30:59 +0000

    controllerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get '/top' => 'homes#top'
+  resources :characters
+  resources :events
+  resources :cards
+  resources :songs
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
